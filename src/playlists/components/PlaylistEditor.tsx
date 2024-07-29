9a3d5374aef94616528ce0542a6384eac78240f0
Mateusz Kulesza <ev45ive@gmail.com>
2024-07-29 13:42:56 +0200

    Exercise: Typing event handlers

--- src/playlists/components/PlaylistEditor.tsx
+++ src/playlists/components/PlaylistEditor.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 const PlaylistEditor = (props: Props) => {
   const playlist = {
@@ -10,31 +10,28 @@ const PlaylistEditor = (props: Props) => {
     description: "Cool playlist",
   };
 
-  // This will render a read-only field.
-  // If the field should be mutable use `defaultValue`. 
-  // Otherwise, set either `onChange` or `readOnly`
-  
+  const changeHandler = (event) => {};
+
   return (
     <div>
       <div className="grid gap-3">
         <div className="grid">
           <label>Name</label>
-          <input type="text" value={playlist.name}  onChange={()=>{}}/>
+          <input type="text" value={playlist.name} onChange={changeHandler} />
         </div>
 
         <div className="grid ">
-          <label className='flex gap-2 items-center'> 
+          <label className="flex gap-2 items-center">
             <input type="checkbox" defaultChecked={playlist.public} /> Public
           </label>
-
         </div>
         <div className="grid">
           <label>Description</label>
-          <textarea value={playlist.description} readOnly={true}/>
+          <textarea value={playlist.description} readOnly={true} />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default PlaylistEditor
+export default PlaylistEditor;
