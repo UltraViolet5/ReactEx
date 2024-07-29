55fed01e1ad119eec98e2ea4eef2c930dd6867df
Mateusz Kulesza <ev45ive@gmail.com>
2024-07-29 14:24:05 +0200

    Exercise: Switching modes

--- src/playlists/containers/PlaylistsView.tsx
+++ src/playlists/containers/PlaylistsView.tsx
@@ -1,32 +1,35 @@
 import React from "react";
 import PlaylistList from "../components/PlaylistList";
-import PlaylistDetails from "../components/PlaylistDetails"; 
+import PlaylistDetails from "../components/PlaylistDetails";
 import PlaylistEditor from "../components/PlaylistEditor";
 
 type Props = {};
 
 const PlaylistsView = (props: Props) => {
+  let mode: "details" | "editor" = "details";
+
+  const showDetails = () => {};
+  const showEditor = () => {};
+
   return (
     <div>
-      {/* div>ul.placki>li.item*3{Item $} */}
-      {/* .grid.grid-cols-2.gap-4>div*2 */}
-
-      <h3 className="text-3xl leading-loose">
-        Playlists
-      </h3>
+      <h3 className="text-3xl leading-loose">Playlists</h3>
 
       <div className="grid grid-cols-2 gap-4">
         <div>
-          <PlaylistList/>
+          <PlaylistList />
         </div>
         <div className="grid gap-5">
-          <PlaylistDetails/>
-          
-          {/* createElement(PlaylistEditor) */}
-          <PlaylistEditor/>
+          <PlaylistDetails />
+
+          {false && <PlaylistEditor />}
+
+          <div className="flex justify-between">
+            <button>Details</button>
+            <button>Editor</button>
+          </div>
         </div>
       </div>
-
     </div>
   );
 };
