af65f3371171f85bd5143639c96bec6632ff578e
Mateusz Kulesza <ev45ive@gmail.com>
2024-07-29 14:58:50 +0200

    Extracting playlists type

--- src/playlists/components/PlaylistList.tsx
+++ src/playlists/components/PlaylistList.tsx
@@ -1,35 +1,17 @@
 import React from "react";
+import { mockPlaylists } from "../../core/model/mockPlaylists";
 
 type Props = {};
-
-const playlists = [
-  {
-    id: "123",
-    name: "Playlist 123",
-    public: false,
-    description: "BEst playlist",
-  },
-  {
-    id: "234",
-    name: "Playlist 234",
-    public: true,
-    description: "Cool playlist",
-  },
-  {
-    id: "345",
-    name: "Playlist 345",
-    public: false,
-    description: "Awesome playlist",
-  },
-];
 
 /* .grid.divide-y-2.divide-gray-500.divide-solid.px-5.py-2 */
 
 const PlaylistList = (props: Props) => {
   return (
     <div className="grid divide-y divide-gray-300 divide-solid">
-      {playlists.map((playlist, index) => (
-        <div className="px-5 py-2">{index+1}. {playlist.name}</div>
+      {mockPlaylists.map((playlist, index) => (
+        <div className="px-5 py-2">
+          {index + 1}. {playlist.name}
+        </div>
       ))}
     </div>
   );
