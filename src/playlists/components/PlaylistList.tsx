cfea34a6d51e1b411bf8dcd5a495ed8fbe8bb3d1
Mateusz Kulesza <ev45ive@gmail.com>
2024-07-29 15:11:53 +0200

    Error: Too many re-renders. React limits the number of renders to prevent an infinite loop.

--- src/playlists/components/PlaylistList.tsx
+++ src/playlists/components/PlaylistList.tsx
@@ -1,22 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { mockPlaylists } from "../../core/model/mockPlaylists";
 
 type Props = {};
 
-/* .grid.divide-y-2.divide-gray-500.divide-solid.px-5.py-2 */
-
 const PlaylistList = (props: Props) => {
+  const [selectedId, setSelectedId] = useState("234");
 
-  const selectedId = '234'
-
-  const selectPlaylistById = (id:string) => {
-    // selectedId = id 
-  }
+  const selectPlaylistById = (id: string) => {
+    setSelectedId(id);
+  };
 
   return (
     <div className="grid divide-y divide-gray-300 divide-solid">
       {mockPlaylists.map((playlist, index) => (
-        <div className="px-5 py-2 bg-blue-500 text-white">
+        <div
+          className={`px-5 py-2 ${
+            playlist.id === selectedId
+              ? "bg-blue-500 text-white"
+              : "cursor-pointer hover:bg-blue-200"
+          }`}
+          // Error: Too many re-renders. React limits the number of renders to prevent an infinite loop.
+          // onClick={selectPlaylistById(playlist.id)}
+          onClick={() => selectPlaylistById(playlist.id)}
+          key={playlist.id}
+        >
           {index + 1}. {playlist.name}
         </div>
       ))}
