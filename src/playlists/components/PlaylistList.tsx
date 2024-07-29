f72e3631352c94f6857de248b4ba3524c2bcfb81
Mateusz Kulesza <ev45ive@gmail.com>
2024-07-29 15:01:02 +0200

    Exercise: selecting playlists

--- src/playlists/components/PlaylistList.tsx
+++ src/playlists/components/PlaylistList.tsx
@@ -6,10 +6,17 @@ type Props = {};
 /* .grid.divide-y-2.divide-gray-500.divide-solid.px-5.py-2 */
 
 const PlaylistList = (props: Props) => {
+
+  const selectedId = '234'
+
+  const selectPlaylistById = (id:string) => {
+    // selectedId = id 
+  }
+
   return (
     <div className="grid divide-y divide-gray-300 divide-solid">
       {mockPlaylists.map((playlist, index) => (
-        <div className="px-5 py-2">
+        <div className="px-5 py-2 bg-blue-500 text-white">
           {index + 1}. {playlist.name}
         </div>
       ))}
