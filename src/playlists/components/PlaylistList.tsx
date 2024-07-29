f002c09456f7657fc9fef2661d83958d00e14dfc
Mateusz Kulesza <ev45ive@gmail.com>
2024-07-29 14:46:34 +0200

    Exercise: Rendering lists

--- src/playlists/components/PlaylistList.tsx
+++ src/playlists/components/PlaylistList.tsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
+
+const playlists = [
+  {
+    id: "123",
+    name: "Playlist 123",
+    public: false,
+    description: "BEst playlist",
+  },
+  {
+    id: "234",
+    name: "Playlist 234",
+    public: true,
+    description: "Cool playlist",
+  },
+  {
+    id: "345",
+    name: "Playlist 345",
+    public: false,
+    description: "Awesome playlist",
+  },
+];
+
+/* .grid.divide-y-2.divide-gray-500.divide-solid.px-5.py-2 */
 
 const PlaylistList = (props: Props) => {
+  
   return (
-    <div>PlaylistList</div>
-  )
-}
+    <div className="grid divide-y divide-gray-300 divide-solid">
+      <div className="px-5 py-2">1. Banana</div>
+      <div className="px-5 py-2">2. Banana</div>
+      <div className="px-5 py-2">3. Banana</div>
+    </div>
+  );
+};
 
-export default PlaylistList
+export default PlaylistList;
