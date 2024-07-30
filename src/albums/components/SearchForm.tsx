1437b313b7696a1380bb8a4a7487117f7e4c0a4f
Mateusz Kulesza <ev45ive@gmail.com>
2024-07-30 17:09:11 +0200

    State vs Ref

--- src/albums/components/SearchForm.tsx
+++ src/albums/components/SearchForm.tsx
@@ -1,15 +1,27 @@
 import { Button } from "primereact/button";
 import { InputText } from "primereact/inputtext";
-import React from "react";
+import React, { useRef, useState } from "react";
 
-type Props = {};
+type Props = {
+  onSearch: (query?: string) => void;
+};
 
-const SearchForm = (props: Props) => {
+const SearchForm = ({ onSearch }: Props) => {
+  const [query, setQuery] = useState("");
+  // const query = useRef("");
+
   return (
     <div>
       <div className="p-inputgroup flex-1">
-        <InputText placeholder="Album search..." />
-        <Button label="Search" />
+        <InputText
+          placeholder="Album search..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          // onChange={(e) => (query.current = e.target.value)}
+        />
+
+        <Button label="Search" onClick={() => onSearch(query)} />
+        {/* <Button label="Search" onClick={() => onSearch(query.current)} /> */}
       </div>
     </div>
   );
--- src/albums/containers/AlbumSearchView.tsx
+++ src/albums/containers/AlbumSearchView.tsx
@@ -6,23 +6,19 @@ import { fetchAlbumSearchResults } from "../../core/services/MusicAPI";
 type Props = {};
 
 const AlbumSearchView = (props: Props) => {
-
-
-  const search = (query='') => {
-    const reusults = fetchAlbumSearchResults(query)
-  }
+  const search = (query = "") => {
+    const reusults = fetchAlbumSearchResults(query);
+  };
 
   return (
     <div className="container">
       <h3 className="text-3xl leading-loose">Search</h3>
 
-      {/* .grid.grid-cols-2>div*2 */}
-
       <div className="grid gap-5">
         <div>
-          <SearchForm />
+          <SearchForm onSearch={search} />
         </div>
-        
+
         <div>
           <ResultsGrid />
         </div>
