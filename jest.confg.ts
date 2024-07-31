3526af24baa2a9e31528959239bc2bbdf718963c
Mateusz Kulesza <ev45ive@gmail.com>
2024-07-31 17:03:39 +0200

    Testing Library + Jest/Vitest + JSDOM

--- jest.confg.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from 'jest';
-
-const config: Config = {
-    "watchPathIgnorePatterns": [
-        "<rootDir>/node_modules",
-    ],
-    rootDir: 'src'
-}
-export default config
--- src/playlists/components/PlaylistDetails.test.tsx
+++ src/playlists/components/PlaylistDetails.test.tsx
@@ -1,6 +1,31 @@
+/**
+ * @vitest-environment jsdom
+ */
+
+import { describe, it, expect, afterEach } from "vitest";
+import PlaylistDetails from "./PlaylistDetails";
+import { cleanup, render, screen } from "@testing-library/react";
+import { mockPlaylists } from "../../core/model/mockPlaylists";
+
 describe("PlaylistDetails", () => {
-  it("should render ", () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  it.skip("should render ", () => {
     expect(true).toBe(true);
   });
+
+  it("should render  No playlist selected ", () => {
+    render(<PlaylistDetails onEdit={() => {}} />);
+    screen.getByText("No playlist selected");
+  });
+
+  it("should render playlist data", () => {
+    render(<PlaylistDetails playlist={mockPlaylists[0]} onEdit={() => {}} />);
+
+    expect(screen.queryByText("No playlist selected")).toBeNull();
+
+    screen.debug(document.body);
+  });
 });
-  
