607c8e4dbadbd18fbd8d8b2a948de39955605aaf
Mateusz Kulesza <ev45ive@gmail.com>
2024-07-31 14:43:08 +0200

    TanStack Query + Global Cache Key

--- src/albums/containers/AlbumSearchView.tsx
+++ src/albums/containers/AlbumSearchView.tsx
@@ -2,12 +2,29 @@ import { useState } from "react";
 import SearchForm from "../components/SearchForm";
 import ResultsGrid from "../components/ResultsGrid";
 import { Loader } from "./Loader";
-import { useAlbumSearchResults } from "./useAlbumSearchResults";
+import { useAlbumSearchResults, useFetch } from "./useAlbumSearchResults";
+import { fetchAlbumSearchResults } from "../../core/services/MusicAPI";
+import { useQuery } from "@tanstack/react-query";
 
 const AlbumSearchView = () => {
   const [query, setQuery] = useState("");
 
-  const { isLoading, error, data: albums = [] } = useAlbumSearchResults(query);
+  // const {
+  //   isLoading,
+  //   error,
+  //   data: albums = [],
+  // } = useFetch(query, fetchAlbumSearchResults, (data) => data.albums.items);
+
+  const {
+    data: albums = [],
+    error,
+    isLoading,
+  } = useQuery({
+    queryKey: ["albums/search:", query], // Cache KEY
+    queryFn: ({ signal }) => fetchAlbumSearchResults(query, { signal }),
+    select: (data) => data.albums.items,
+    enabled: () => query !== "",
+  });
 
   return (
     <div className="container">
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { PrimeReactProvider } from "primereact/api";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const client = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode> {/* // Run Effects 2x */} 
-    <PrimeReactProvider>
-      <App />
-    </PrimeReactProvider>
+  <React.StrictMode>
+    <QueryClientProvider client={client}>
+      <PrimeReactProvider>
+        <App />
+      </PrimeReactProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
